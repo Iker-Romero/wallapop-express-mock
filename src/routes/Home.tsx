dead072d505b366380c9c1e8f798d38348ddf66e
Iker-Romero <ikersoftdev@gmail.com>
2022-11-13 18:26:46 +0100

    Carousel content data imported from another file

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,131 +10,6 @@ import data from '../data';
 import theme from '../theme';
 
 const Home = () => {
-  const categories = [
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_All.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_MotorAccessories.svg',
-      title: 'Todas las categorías',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Cars.svg',
-      hover: 'https://es.wallapop.com/images/icons/categories/category_Cars_hover.svg',
-      title: 'Coches',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Motorbike.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_Motorbike_hover.svg',
-      title: 'Motos',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_MotorAccessories.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_MotorAccessories_hover.svg',
-      title: 'Motos y Accesorios',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_All.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_FashionAccessories_hover.svg',
-      title: 'Moda y Accesorios',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_RealEstate.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_RealEstate_hover.svg',
-      title: 'Inmobiliaria',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_TVAudioCameras.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_TVAudioCameras_hover.svg',
-      title: 'TV, Audio y Foto',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_CellPhonesAccessories.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_CellPhonesAccessories_hover.svg',
-      title: 'Móviles y Telefonía',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_ComputersElectronic.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_ComputersElectronic_hover.svg',
-      title: 'Informática y Electrónica',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_SportsLeisure.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_SportsLeisure_hover.svg',
-      title: 'Deporte y Ocio',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Bikes.svg',
-      hover: 'https://es.wallapop.com/images/icons/categories/category_Bikes_hover.svg',
-      title: 'Bicicletas',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_GamesConsoles.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_GamesConsoles_hover.svg',
-      title: 'Hogar y Jardín',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Appliances.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_Appliances_hover.svg',
-      title: 'Electrodomésticos',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_GamesBooks.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_GamesBooks_hover.svg',
-      title: 'Cine, Libros y Música',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_BabiesChild.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_BabiesChild_hover.svg',
-      title: 'Niños y Bebés',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_CollectiblesArt.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_CollectiblesArt_hover.svg',
-      title: 'Coleccionismo',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Construction.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_Construction_hover.svg',
-      title: 'Construcción y Reformas',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_AgricultureIndustrial.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_AgricultureIndustrial_hover.svg',
-      title: 'Industria y Agricultura',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Jobs.svg',
-      hover: 'https://es.wallapop.com/images/icons/categories/category_Jobs_hover.svg',
-      title: 'Empleo',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Services.svg',
-      hover:
-        'https://es.wallapop.com/images/icons/categories/category_Services_hover.svg',
-      title: 'Servicios',
-    },
-    {
-      img: 'https://es.wallapop.com/images/icons/categories/category_Others.svg',
-      hover: 'https://es.wallapop.com/images/icons/categories/category_Others_hover.svg',
-      title: 'Otros',
-    },
-  ];
-
   return (
     <>
       <P>Wallapop, la plataforma líder de compraventa de productos de segunda mano</P>
@@ -147,7 +22,7 @@ const Home = () => {
         textAlign="center"
       >
         <H1>¿Qué estás buscando hoy?</H1>
-        <Carousel cards={categories} cardWidth="5rem" />
+        <Carousel cards={data.categories} cardWidth="5rem" />
       </Div>
       <Div
         display="flex"
