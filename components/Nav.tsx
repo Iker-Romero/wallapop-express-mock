dd6db738c27e3e42d8997dc00874e2bd359b540c
Iker-Romero <ikersoftdev@gmail.com>
2022-11-14 01:46:44 +0100

    Nav component imports reduced

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,6 @@
-import { useState } from 'react';
 import { memo } from 'react';
-import { NavLink } from 'react-router-dom';
 
-import Image from '../components/Image';
-import Login from '../components/Login';
-import PopUp from '../components/PopUp';
-import Div from '../components-ui/Div';
 import NavStyle from '../components-ui/StyledNav';
-import theme from '../src/theme';
-import Button from './Button';
-import Input from './Input';
 
 const Nav = ({ children }) => {
   return <NavStyle>{children}</NavStyle>;
